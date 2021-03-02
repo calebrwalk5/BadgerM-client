c6be7740efd9ae00ae03c1147c8a11aff54afb2c
Caleb W <51061101+calebrwalk5@users.noreply.github.com>
2021-03-02 14:44:07 -0500

    Add files via upload

---
+++ BadgerM.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <windows.h>
+
+/*
+ * BadgerM Launcher
+ * Written by calebrwalk5
+*/
+
+int main() {
+	system ("start index.exe");
+	printf("Start GTA V/n");
+return 0;
+}
+
+
---
+++ initial_setup.bat
@@ -0,0 +1,5 @@
+@echo off
+title Initial BadgerM Setup
+color 1F
+echo Setting up BadgerM
+
